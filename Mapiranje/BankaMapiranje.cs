6f538ee003c0c9e373d83a65aa0999eb684d104d
f1cejfvr <filipjovanovic107@gmail.com>
2025-07-18 21:06:12 +0200

    dodao sam folder mapiranje

---
+++ Mapiranje/BankaMapiranje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg.XmlHbmBinding;
+using proejkt.Entiteti;
+using FluentNHibernate.Mapping;
+
+namespace proejkt.Mapiranje
+{
+    public class BankaMapiranje : ClassMap<Banka>
+    {
+
+    }
+}
