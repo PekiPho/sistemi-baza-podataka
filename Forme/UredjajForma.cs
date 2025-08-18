0e8ff08c35878bb2ac46b056b526b6df9c962d8d
f1cejfvr <filipjovanovic107@gmail.com>
2025-08-18 18:30:30 +0200

    dodata glavna forma

---
+++ Forme/UredjajForma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proejkt.Forme
+{
+    public partial class UredjajForma : Form
+    {
+        public UredjajForma()
+        {
+            InitializeComponent();
+        }
+    }
+}
